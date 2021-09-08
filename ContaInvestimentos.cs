677562a02b793bbbf856de17738ff273a5b68cf4
Tiago Almeida <tiagoalmeida48@yahoo.com.br>
2021-09-07 21:06:32 -0300

    Add files via upload

---
+++ ContaInvestimentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace banco
+{
+    class ContaInvestimentos : Conta
+    {
+        public double SacarDinheiroInvestimento(double valor)
+        {
+            return saldoAtual -= valor;
+        }
+
+        public double DepositarDinheiroInvestimento(double valor)
+        {
+            return saldoAtual += RenderJuros(valor);
+        }
+
+        public double RenderJuros(double valor)
+        {
+            return saldoAtual += valor * 1.02;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            ClientePF cliPF = new ClientePF();
+            cliPF.nome = "Tiago";
+            double valor = 30;
+            conta.DepositarDinheiro(valor);
+
+            Console.WriteLine("Depósito realizado de R$ " + valor + " com sucesso!");
+            Console.WriteLine("Nome:" + cliPF.nome);
+            Console.WriteLine("Valor atual: R$ " + conta.saldoAtual);
+            Console.WriteLine();
+              
+            conta.SacarDinheiro(30);
+            Console.WriteLine("Saque realizado de R$ " + valor + " com sucesso");
+            Console.WriteLine("Nome:" + cliPF.nome);
+            Console.WriteLine("Valor atual: R$" + conta.saldoAtual);
+        }
+    }
+}
